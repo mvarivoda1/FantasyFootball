e5007a99a905d48ffee23398fb53be4b60a57efe
Marijan Varivoda <marijanvarivoda13@gmail.com>
2026-03-26 12:03:14 +0100

    initial commit

---
+++ FantasyFootball/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FantasyFootball.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
